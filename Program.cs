8ef72ddbf3587ab2d5004bebea9bca97cbfe909d
Lucas Alves <lucasalves94@gmail.com>
2024-12-17 22:48:45 -0300

    added binary search for string too

--- Program.cs
+++ Program.cs
@@ -26,13 +26,76 @@
         return null;
     }
 
+    public int? SimpleBinarySearchForString(string[] list, string target)
+    {
+        int lowGuess = 0;
+        int highGuess = list.Length - 1;
+
+        while (lowGuess <= highGuess)
+        {
+            int middle = (lowGuess + highGuess) / 2;
+            string guess = list[middle];
+
+            int comparison = string.Compare(guess, target, StringComparison.OrdinalIgnoreCase);
+
+            if (comparison == 0) 
+            {
+                return middle;
+            }
+            else if (comparison > 0) 
+            {
+                highGuess = middle - 1;
+            }
+            else 
+            {
+                lowGuess = middle + 1;
+            }
+        }
+
+        return null; 
+    }
+
     public static void Main()
     {
         int[] my_list = { 1, 3, 5, 7, 9 };
 
         BinarySearch search = new BinarySearch();
-        int? result = search.SimpleBinarySearch(my_list, 7);
+        int? result = search.SimpleBinarySearch(my_list, 4);
 
         Console.WriteLine(result.HasValue ? $"Item found at index: {result}" : "Item not found");
+
+
+        string[] names =
+    {
+        "Alice", "Bob", "Charlie", "Diana", "Eve", "Frank", "Grace", "Hank", "Ivy", "Jack",
+        "Karen", "Liam", "Mona", "Nate", "Olivia", "Paul", "Quincy", "Rachel", "Sam", "Tina",
+        "Uma", "Victor", "Wendy", "Xander", "Yara", "Zane", "Anna", "Ben", "Cara", "David",
+        "Ella", "Finn", "Gina", "Harry", "Isla", "James", "Kara", "Leo", "Mia", "Noah",
+        "Oscar", "Piper", "Quinn", "Riley", "Sophie", "Thomas", "Ursula", "Violet", "Will", "Xena",
+        "Yuri", "Zara", "Adrian", "Bella", "Caleb", "Daisy", "Ethan", "Fiona", "Gavin", "Hazel",
+        "Ian", "Julia", "Kyle", "Lila", "Mason", "Nina", "Owen", "Phoebe", "Quinton", "Ruby",
+        "Simon", "Tara", "Ulysses", "Vanessa", "Wyatt", "Xiomara", "Yvette", "Zack", "Aiden", "Brooke",
+        "Chase", "Delilah", "Elliot", "Faith", "George", "Holly", "Isaac", "Jade", "Kieran", "Lydia",
+        "Miles", "Natalie", "Oliver", "Paige", "Quentin", "Rebecca", "Sebastian", "Taylor", "Ulrich", "Vivian",
+        "Walker", "Xander", "Yvonne", "Zion", "Arthur", "Bridget", "Cody", "Danielle", "Edward", "Felicity",
+        "Grant", "Hannah", "Iris", "Justin", "Kayla", "Logan", "Megan", "Nathan", "Orla", "Parker",
+        "Quinlan", "Rose", "Scott", "Tracy", "Uma", "Victor", "Willow", "Ximena", "Yves", "Zane"
+    };
+        Array.Sort(names);
+
+
+        string target = "Justin";
+        int? resultString = search.SimpleBinarySearchForString(names, target);
+
+        if (resultString.HasValue)
+        {
+            Console.WriteLine($"Name '{target}' found at index: {resultString}");
+        }
+        else
+        {
+            Console.WriteLine($"Name '{target}' not found.");
+        }
+
+
     }
 }
--- obj/Debug/net8.0/binary-search.AssemblyInfo.cs
+++ obj/Debug/net8.0/binary-search.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("binary-search")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06bd21e76e7a0b9d6f91393a38d9a4ed0fb6d991")]
 [assembly: System.Reflection.AssemblyProductAttribute("binary-search")]
 [assembly: System.Reflection.AssemblyTitleAttribute("binary-search")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
